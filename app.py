08a69b2bb10d3355158f328c6a27bb588ff48d19
mac <mac@macteki-MacBook-Pro.local>
2018-12-19 00:30:53 +0800

    Hi

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from bottle import route, run, request, abort, static_file
 
 from fsm import TocMachine
-
+import os
 
 VERIFY_TOKEN = "123"
+PORT = os.environ['PORT']
 
 machine = TocMachine(
     states=[
@@ -127,4 +128,4 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=5000, debug=True, reloader=True)
+    run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
