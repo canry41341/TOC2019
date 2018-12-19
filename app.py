e06c519e74041eabe7a2ad9a3375cc8a2dd20dfa
mac <mac@macteki-MBP.mshome.net>
2018-12-19 13:12:20 +0800

    QQQQQQ

--- app.py
+++ app.py
@@ -134,10 +134,10 @@ def webhook_handler():
             d[sender_id] = machine.state
 
         if machine.state == "user":
-           # text = event['message']['text']
+            text = event['message']['text']
             if text.lower() != '你好' and text.lower() != '介紹' and text.lower() != '減肥' and text.lower() != '即時新聞' and text != '1' and text != '2':
                 print("I AM IN!!")
-                rr = requests.get(google_url, params = text)
+                rr = requests.get(google_url, params = {'q':'寒流'})
                 if rr.status_code == requests.codes.ok:
                     soup = BeautifulSoup(rr.text,'html.parser')
                     items = soup.select('div.g > h3.r > a[href^="/url"]')
